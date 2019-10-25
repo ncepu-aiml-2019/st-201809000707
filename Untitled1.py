88bbd15c0c223b8dcaa928ca9d0ba3a79aaac7bf
unknown <614552400@qq.com>
2019-10-25 13:42:19 +0800

    Untitled1.

---
+++ Untitled1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+num=[];
+i=2
+for i in range(2,100):
+   j=2
+   for j in range(2,i):
+      if(i%j==0):
+         break
+   else:
+      num.append(i)
+print(num)
+
+
+# In[ ]:
+
+
+
+
